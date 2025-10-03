096f3ad81195882188af457fdb292f0cf44015dc
Saloni Shah <59715228+shahsalonik@users.noreply.github.com>
2025-10-03 15:05:57 -0400

    Time: 3 ms (49.93%) | Memory: 19.1 MB (23.86%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        targ_map = {}
+        targ_map = {} # map for num to index
 
         for i, val in enumerate(nums):
             need = target - val
-            if need in targ_map:
-                return [i, targ_map[need]]
-
+            if need in targ_map: # ensures you don't visit the same index
+                return [i, targ_map[need]] # will return in inverted order
             targ_map[val] = i
-        return [-1, -1]
+
+        return [-1, -1] # should never reach
