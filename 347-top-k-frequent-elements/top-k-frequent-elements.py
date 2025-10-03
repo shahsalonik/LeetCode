9fdea0876f8851bef18a574969aaab5f33e541f3
Saloni Shah <59715228+shahsalonik@users.noreply.github.com>
2025-10-03 15:25:52 -0400

    Time: 4 ms (69.14%) | Memory: 21.4 MB (40.15%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,11 +1,13 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        from collections import Counter
-        nums_dict = Counter(nums)
+        freq = Counter(nums)
+        sorted_items_desc = sorted(freq.items(), key=lambda item: item[1], reverse=True)
+
         result = []
-        # this is how you sort a dictionary in python in descending order
-        output = dict(sorted(nums_dict.items(), key=lambda item: item[1], reverse=True))
-        key_list = list(output.keys())
-        while len(result) < k:
-            result.append(key_list[len(result)])
+
+        for elem in sorted_items_desc:
+            if len(result) >= k:
+                break
+            result.append(elem[0])
+
         return result
