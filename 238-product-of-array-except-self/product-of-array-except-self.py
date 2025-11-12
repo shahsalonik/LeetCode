3775abc5e9e973c9aabdb903721af3d48f237939
Saloni Shah <59715228+shahsalonik@users.noreply.github.com>
2025-11-12 10:10:31 -0500

    Time: 26 ms (50.33%) | Memory: 23.1 MB (94.03%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,23 +1,20 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        result = []
-        total_product = 1
-        total_product_no_zero = 1
-        zero_count = 0
+        result = [1] * (len(nums)) # result array
+        # if nums is [1, 2, 4, 6]
 
-        for num in nums:
-            total_product *= num
-            if num != 0:
-                total_product_no_zero *= num
-            if num == 0:
-                zero_count += 1
-
+        prefix = 1 
+        # iterate through the array forwards
+        # calculate product for everything that comes before
         for i in range(len(nums)):
-            if nums[i] == 0 and zero_count <= 1:
-                result.append(total_product_no_zero)
-            elif nums[i] == 0 and zero_count > 1:
-                result.append(total_product)
-            else:
-                result.append(int(total_product / nums[i]))
-
+            result[i] = prefix # 1 # 1 # 2 # 8
+            prefix *= nums[i]  # 1 # 2 # 8 # 48
+        # result = [1, 1, 2, 8]
+        suffix = 1
+        # iterate through the array backwards
+        # multiply it through backwards
+        for i in range(len(nums) - 1, -1, -1):
+            result[i] *= suffix
+            suffix *= nums[i] # 6, 24, 48
         return result
+        # result = [48, 24, 12, 8]
