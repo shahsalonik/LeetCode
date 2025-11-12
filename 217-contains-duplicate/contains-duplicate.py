72bc6ad50cf601997fb7eafe61ac9d4053d91d6d
Saloni Shah <59715228+shahsalonik@users.noreply.github.com>
2025-11-12 09:48:41 -0500

    Time: 3 ms (98.98%) | Memory: 31.5 MB (53.80%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,13 +1,3 @@
 class Solution:
-    # from collections import Counter
     def containsDuplicate(self, nums: List[int]) -> bool:
-
         return len(set(nums)) != len(nums)
-
-        # freq = Counter(nums)
-
-        # for key, val in freq.items():
-        #     if val > 1:
-        #         return True
-
-        # return False
