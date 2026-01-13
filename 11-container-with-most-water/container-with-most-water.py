b18c27044f9cb4f86859b12f532704f2f382da1f
Saloni Shah <59715228+shahsalonik@users.noreply.github.com>
2026-01-13 11:58:07 -0500

    Time: 63 ms (86.46%) | Memory: 29.6 MB (9.32%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -2,22 +2,17 @@ class Solution:
     def maxArea(self, height: List[int]) -> int:
         left = 0
         right = len(height) - 1
-        length = right - left
-        curr_area = min(height[left], height[right]) * length
+        max_area = 0
 
-        # this loop condition can't be length > 0
-        # this is bc it's the pointers that are changing
-        # not just the length (if the pointers change, the length does too)
         while left < right:
             length = right - left
-            total = min(height[left], height[right]) * length
-            if total > curr_area:
-                curr_area = total
-            elif height[left] < height[right]:
+            heights = min(height[left], height[right])
+            volume = length * heights
+            max_area = max(volume, max_area)
+
+            if height[left] <= height[right]:
                 left += 1
-            elif height[right] < height[left]:
-                right -= 1
             else:
-                left += 1
                 right -= 1
-        return curr_area
+
+        return max_area
