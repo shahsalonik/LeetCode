2fe3ad9be8470d4101b03a3d6ddd713365befaae
Saloni Shah <59715228+shahsalonik@users.noreply.github.com>
2026-05-17 14:10:33 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (40.55%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,18 +1,25 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        row = 0
-        left = 0
-        right = len(matrix[row]) - 1
-        while row < len(matrix) and target > matrix[row][right]:
-            row += 1
-        if row >= len(matrix):
-            return False
+        # find the row to search in
+
+        # possible to filter by checking the last elem of the row and seeing if > target?
+
+        left, right, row = 0, len(matrix[0]) - 1, 0
+
+        for i in range(len(matrix)):
+            if matrix[i][right] >= target:
+                if matrix[i][right] == target:
+                    return True
+                row = i
+                break
+        
         while left <= right:
-            middle = (left + right) // 2
-            if target == matrix[row][middle]:
+            mid = (left + right) // 2
+            if matrix[row][mid] == target:
                 return True
-            elif target > matrix[row][middle]:
-                left = middle + 1
+            elif matrix[row][mid] > target:
+                right = mid - 1
             else:
-                right = middle - 1
+                left = mid + 1
+        
         return False
